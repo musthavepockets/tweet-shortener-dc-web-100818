fc5c2eef00fdb3d9adb7cbb696c99e76ba96e116
cd <coraldarling@outlook.com>
2018-09-30 15:53:06 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,19 @@
-# Write your code here.
+require "pry"
+
+def dictionary
+  {"hello" => "hi", "to" => "2", "two" => "2", "too" => "2", "for" => "4", "four" => "4", "be" => "b", "you" => "u", "at" => "@", "and" => "&"}
+end
+
+
+def word_substituter(tweet)
+  tweet_array = tweet.split
+  tweet_array.each do |word|
+    dictionary.each do |key, value|
+      if dictionary.keys.include?(word) == true
+        word = dictionary[key]
+      end
+    end
+  tweet_array.join(" ")
+  binding.pry
+  end
+end
